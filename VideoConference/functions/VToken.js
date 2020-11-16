79b775ebb57571ddf6be7fdb1511de0208ce872f
Saravanan raghul <saravananraghul1820@gmail.com>
2020-11-16 14:05:47 +0530

    Added new const.

--- VideoConference/functions/VToken.js
+++ VideoConference/functions/VToken.js
@@ -6,6 +6,15 @@ exports.handler = function (context, event, callback) {
 
     const accessToken = new AccessToken(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET);
 
+    accessToken.identity = event.identity;
+
+    const VideoGrant = AccessToken.VideoGrant;
+    const videoGrant = new VideoGrant({
+        room: event.room
+    });
+
+    accessToken.addGrant(videoGrant);
+
     callback(null, {
         token: accessToken.toJwt()
     });
