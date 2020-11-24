c8909e6e2ec99c4e85cc25df2ca0cb7c05c0f85b
Saravanan raghul <saravananraghul1820@gmail.com>
2020-11-24 12:18:08 +0530

    fetched JwtToken

--- VideoConference/functions/VToken.js
+++ VideoConference/functions/VToken.js
@@ -1,21 +1,28 @@
-exports.handler = function (context, event, callback) {
-    const TWILIO_ACCOUNT_SID = context.ACCOUNT_SID;
-    const TWILIO_API_KEY = context.API_KEY;
-    const TWILIO_API_SECRET = context.API_SECRET;
-    const AccessToken = Twilio.jwt.AccessToken;
+exports.handler = function(context, event, callback) {
+	const TWILIO_ACCOUNT_SID = context.ACCOUNT_SID;
+	const TWILIO_API_KEY = context.API_KEY;
+	const TWILIO_API_SECRET = context.API_SECRET;
+	const AccessToken = Twilio.jwt.AccessToken;
 
-    const accessToken = new AccessToken(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET);
+	const accessToken = new AccessToken(TWILIO_ACCOUNT_SID, TWILIO_API_KEY, TWILIO_API_SECRET);
 
-    accessToken.identity = event.identity;
+	accessToken.identity = event.identity;
 
-    const VideoGrant = AccessToken.VideoGrant;
-    const videoGrant = new VideoGrant({
-        room: event.room
-    });
+	const VideoGrant = AccessToken.VideoGrant;
+	const videoGrant = new VideoGrant({
+		room: event.room
+	});
 
-    accessToken.addGrant(videoGrant);
+	accessToken.addGrant(videoGrant);
 
-    callback(null, {
-        token: accessToken.toJwt()
-    });
+	let response = new Twilio.Response();
+	const headers = {
+		'Access-Control-Allow-Origin': '*',
+		'Access-Control-Allow-Methods': 'GET, POST',
+		'Access-Control-Allow-Headers': 'Content-Type'
+	};
+	response.setHeaders(headers);
+	response.setBody(accessToken.toJwt());
+
+	callback(null, response);
 };
